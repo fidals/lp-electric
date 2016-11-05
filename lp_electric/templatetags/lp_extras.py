798a3b6181bb2d8ed4152677fff6f57ccbfc3e78
duker33 <duker33@gmail.com>
2016-11-05 12:21:39 +0300

    [817] Remove random page enabling

--- lp_electric/templatetags/lp_extras.py
+++ lp_electric/templatetags/lp_extras.py
@@ -8,18 +8,3 @@ register = template.Library()
 @register.assignment_tag
 def roots():
     return Category.objects.root_nodes().order_by('page__position')
-
-
-@register.assignment_tag
-def resolve_url(request):
-    return resolve(request.path_info)
-
-
-@register.assignment_tag
-def translated_path(request, resolve_match):
-    url_name = (
-        (resolve_match.namespace + ':')
-        if resolve_match.namespace else '' +
-        resolve_match.url_name
-    )
-    return reverse(url_name, args=resolve_match.args, kwargs=resolve_match.kwargs)
