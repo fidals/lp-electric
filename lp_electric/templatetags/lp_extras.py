e60706f4779a5cfa2f1020ed5cb539a291aa0ef9
duker33 <duker33@gmail.com>
2016-11-05 12:07:37 +0300

    [817] Try to enable multilang on every page

--- lp_electric/templatetags/lp_extras.py
+++ lp_electric/templatetags/lp_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.urlresolvers import resolve, reverse
 from lp_electric.models import Category
 
 register = template.Library()
@@ -7,3 +8,18 @@ register = template.Library()
 @register.assignment_tag
 def roots():
     return Category.objects.root_nodes().order_by('page__position')
+
+
+@register.assignment_tag
+def resolve_url(request):
+    return resolve(request.path_info)
+
+
+@register.assignment_tag
+def translated_path(request, resolve_match):
+    url_name = (
+        (resolve_match.namespace + ':')
+        if resolve_match.namespace else '' +
+        resolve_match.url_name
+    )
+    return reverse(url_name, args=resolve_match.args, kwargs=resolve_match.kwargs)
