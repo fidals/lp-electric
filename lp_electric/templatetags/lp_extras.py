78e80ad25cce05afe05dd7c472569168f6906d31
duker33 <duker33@gmail.com>
2016-11-06 15:22:56 +0300

    [818] Add language switching to random page

--- lp_electric/templatetags/lp_extras.py
+++ lp_electric/templatetags/lp_extras.py
@@ -8,3 +8,19 @@ register = template.Library()
 @register.assignment_tag
 def roots():
     return Category.objects.root_nodes().order_by('page__position')
+
+
+@register.assignment_tag
+def resolve_url(request):
+    return resolve(request.path_info)
+
+
+@register.assignment_tag
+def reverse_url(resolve_match):
+    m = resolve_match
+    url_name = (
+        m.namespace + ':' + m.url_name
+        if m.namespace
+        else m.url_name
+    )
+    return reverse(url_name, args=m.args, kwargs=m.kwargs)
